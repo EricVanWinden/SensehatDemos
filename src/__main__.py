f47ed782380989a051f9729d9092114f5f8fe500
ericvanwinden <ericvanwinden@hotmail.com>
2025-12-14 22:27:04 +0100

    fix

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,4 @@
 import logging.config
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.insert(0, current_dir)
-
 from advanced_rainbow import AdvancedRainbow
 
 if __name__ == "__main__":
