b25340cd55398cfa905744c2c43f5aa039bbc41b
ericvanwinden <ericvanwinden@hotmail.com>
2025-12-14 22:17:15 +0100

    fix

--- src/__main__.py
+++ src/__main__.py
@@ -8,8 +8,8 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
-from advanced_rainbow import AdvancedRainbow
 from xyz import *
+from advanced_rainbow import AdvancedRainbow
 
 if __name__ == "__main__":
     rb = AdvancedRainbow()
