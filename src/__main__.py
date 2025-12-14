ec7db4ed1e38cdff646017c3c03e67b61570deca
ericvanwinden <ericvanwinden@hotmail.com>
2025-12-14 22:24:59 +0100

    added logging

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,15 @@
-import logging
 import logging.config
-import json
-import sys
 import os
+import sys
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
-from xyz import *
 from advanced_rainbow import AdvancedRainbow
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(module)s - %(funcName)s - %(levelname)s - %(message)s')
     rb = AdvancedRainbow()
     rb.run()
-    
